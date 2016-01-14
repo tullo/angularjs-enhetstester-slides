aee3c92be430ac0bc63591fc2771d236d73a6e3f
Andreas Amstutz <andreasamstutz@gmail.com>
2016-01-14 16:53:40 +0100

    Update karma configs

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,24 @@
 // Karma configuration
-// Generated on Sat Mar 01 2014 06:27:40 GMT+0100 (CET)
+// Generated on Thu Jan 14 2016 15:48:20 GMT+0100 (CET)
 
 module.exports = function(config) {
   config.set({
 
-    // base path, that will be used to resolve files and exclude
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    plugins: [
+      'karma-mocha',
+      'karma-chai-sinon',
+      'karma-jasmine',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-coverage'
+    ],
 
     // frameworks to use
-    frameworks: ['mocha','chai-sinon'],
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai-sinon'],
 
 
     // list of files / patterns to load in the browser
@@ -27,25 +36,23 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      
     ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress','coverage'],
-
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
+      // source files, that you wanna generate coverage for. Do not include tests or libraries
       // (these files will be instrumented by Istanbul)
       'app.js': ['coverage']
     },
 
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress','coverage'],
+
 
     // web server port
     port: 9876,
@@ -57,30 +64,24 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['PhantomJS'],
-
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
-};
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultanous
+    concurrency: Infinity
+  })
+}
